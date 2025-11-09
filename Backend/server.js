dde0ca7ff23401bc518c83a1846e6c7aa7d971c6
anasAbdullfattah <anasAbdullfattah@gmail.com>
2025-11-09 18:30:38 +0200

    Models, middleware, and routes are completed

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,71 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+app.use(express.json());
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please Provide all fields" });
+  }
 
-app.get("/", (req, res) => {
-  res.send("Server is Ready");
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    return res.status(201).json({ success: true, message: "Done" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: "server error" });
+  }
 });
-app.listen(PORT, () => {
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "chech the id again" });
+  }
+  try {
+    const updatedProducts = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    return res.status(200).json({ success: true, data: updatedProducts });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: "server error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    return res.status(201).json({ success: true, message: "Done" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+
+    return res.status(201).json({ success: true, data: products });
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ success: false, message: "server error" });
+  }
+});
+app.listen(process.env.PORT, () => {
   console.log("the server is running");
+  connectDB();
 });
